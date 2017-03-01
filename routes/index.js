d7b2c972df6c1d3b18cb6baf294dec6ebf7c8302
LAPTOP-DEVIN\Devin <dfcm15@mun.ca>
2017-02-28 19:14:55 -0800

    Sample api and login call

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+    if (req.body.user == "test") {
+        if (req.body.pass == "123") {
+            res.render('index', {title: 'test'});
+        }
+    }
+});
+
 module.exports = router;
