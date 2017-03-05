79860a0abd3f1e3d6eaafebbdb646836d79e3816
LAPTOP-DEVIN\Devin <dfcm15@mun.ca>
2017-03-05 11:00:50 -0800

    Added: - API hooks for add/get/remove friends - Began fully completing API functions with /api/post/removeFriend. Looking to make all API functions output to console, and give result jsons as well

--- routes/api.js
+++ routes/api.js
@@ -83,4 +83,52 @@ router.post('/post/register', function(req, res, next) {
     res.redirect('/');
 });
 
+//GET list of friends from userId
+router.get('/get/friends', function(req, res, next) {
+    if (req.query.userId) {
+        DB.friends_findAllFriendsForUser(req.query.userId, function(result) {
+            res.json(result[0]);
+        });
+    }
+});
+
+//POST add friendId to userId
+router.post('/post/addFriend', function(req, res, next) {
+    if (req.body.userId && req.body.friendId) {
+        DB.friends_addFriendToUser(req.body.userId, req.body.friendId, function(result) {
+            console.log(result);
+            res.json(JSON.stringify(result));
+        });
+    }
+});
+
+//POST remove friendId from userId
+//Return json response
+//Result: xy, where x is 0 for no error, 1 for error, y is 0 for successful deletion, 1 for unsuccessful deletion
+router.post('/post/removeFriend', function(req, res, next) {
+    //Declare body variables
+    var userId = req.body.userId;
+    var friendId = req.body.friendId;
+    //Check if body variables are not null, or undefined
+    if (userId && friendId) {
+        DB.friends_deleteFriendFromUser(userId, friendId, function(result) {
+            var dbResult = result.result;
+            //Check for database error
+            if (dbResult == null) {
+                console.log("\x1b[31m%s\x1b[0m%s", "[API]",  " /post/removeFriend: userId: " + userId + ", friendId: " + friendId + ", dbResult: " + JSON.stringify(dbResult));
+                res.json({result: "10", operation: "deleteFriend", text: "Database error"});
+            }   
+            else {
+                console.log("\x1b[32m%s\x1b[0m%s", "[API]",  " /post/removeFriend: userId: " + userId + ", friendId: " + friendId + ", dbResult: " + JSON.stringify(dbResult));
+                if (dbResult.ok == 1 && dbResult.nModified == 1) {
+                    res.json({result: "00", operation: "deleteFriend", text: "Removed friend from user"});
+                }
+                else if (dbResult.ok == 1 && dbResult.nModified == 0) {
+                    res.json({result: "01", operation: "deleteFriend", text: "Could not remove friend from user"});
+                }   
+            }  
+        });
+    }
+});
+
 module.exports = router;
