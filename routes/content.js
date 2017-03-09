04f36ff9678be86748ece821e59b536fa52ad5a4
John Hollett <g1xb17@gmail.com>
2017-03-09 18:48:33 -0330

    Added functions to utils.js and content.js and documented all of it

--- routes/content.js
+++ routes/content.js
@@ -1,84 +1,48 @@
 var express = require('express');
 var router = express.Router();
-var fs = require('fs-extra');
 var path = require('path');
-var mime = require('mime');
-var gm = require('gm');
-/* GET upload page. */
+var utils = require('../utils/utils');
+
+/**
+ * Return User profile picture named 'profile'
+ * HTMLImage Example: src='/content/image/user/user123'
+ */
 router.get('/image/profile/:uid', function(req, res, next) {
-	var file = findFile('profile',path.join(__dirname, '../content/images/user/'+req.params.uid));
-	download(req,res,file);
+	var file = utils.findFiles('profile',path.join(__dirname, '../content/images/user/'+req.params.uid)).next().value;
+	utils.download(req,res,file);
 });
+/**
+ * Return Group profile picture named 'group'
+ * HTMLImage Example: src='/content/image/group/group123'
+ */
 router.get('/image/group/:gid', function(req, res, next) {
-	var file = path.join(__dirname, '../content/images/user/'+req.params.uid+'/profile.jpg');
+	var file = findFiles('group',path.join(__dirname, '../content/images/group/'+req.params.gid)).next().value;
 	download(req,res,file);
 });
 
-router.post('/image/user/:uid', function(req, res, next) {
-	uploadImage(req, 'profile', 'user/'+req.params.uid);
+/**
+ * Upload a User profile image based on the userid
+ * HTMLForm Example: action='/content/image/user/user123'
+ */
+router.post('/image/profile/:uid', function(req, res, next) {
+	utils.uploadImage(req, 'profile', 'user/'+req.params.uid);
     res.redirect('back');           
 });
+
+/**
+ * Upload a Group profile image base don the groupid
+ * HTMLForm Example: action='/content/image/group/test123' 
+ */
 router.post('/image/group/:gid', function(req, res, next) {
-	uploadImage(req, 'group/'+req.params.gid);
+	utils.uploadImage(req, 'group/'+req.params.gid);
     res.redirect('back');   
 });
+
+/* Unused 
 router.post('/image/post/:pid', function(req, res, next) {
 	uploadImage(req, 'posts/'+req.params.pid);
     res.redirect('back');   
 });
-function findFile(filter,directory) {
-	if(!fs.existsSync(directory)) {
-		console.log("ERR: "+directory+" does not exist");
-		return;
-	}
-	var files = fs.readdirSync(directory);
-	for (var i = 0; i < files.length; ++i) {
-		var filename = path.join(directory,files[i]);
-		console.log(filename);
-		var stat = fs.lstatSync(filename);
-		if(!stat.isDirectory() && filename.indexOf(filter) >= 0) {
-			return filename;
-		}
-	}
-	return undefined;
-}
-function download(req,res, file){
-	fs.exists(file,	function(exist){
-		if(exist){
-			var filename = path.basename(file);
-			var mimetype = mime.lookup(file);
-			res.setHeader('Content-disposition','attachment; filename=',filename);
-			res.setHeader('Content-type',mimetype);			
-			var fstream = fs.createReadStream(file);
-			fstream.pipe(res);
-		} else {
-			res.status(404).send('Content not found');
-		}
-	});
-}
-function uploadImage(req, name, dest){
-	var fstream;
-    req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldname, file, filename) {
-    	var dir = path.join(__dirname, '../content/images/' + dest + '/');
-    	var fext = path.extname(filename);
-        console.log("Uploading: " + filename);
-        //Path where image will be uploaded
-        fs.exists(dir,function(exist){
-        	if(!exist)
-        		fs.mkdirSync(dir);
-        	var fileExist = findFile(name, dir);
-        	if (fileExist !== undefined)
-        		fs.unlinkSync(fileExist);
-        	//fstream = fs.createWriteStream(path.join(dir,filename));
-        	//gm(file,filename).write(fstream,function(err) {if (err) throw err;});
-            fstream = fs.createWriteStream(path.join(dir, name + fext));
-            file.pipe(fstream);
-            fstream.on('close', function () {    
-                console.log("Uploaded: " + filename); 
-            });
-        });
-    });
-}
+*/
 
 module.exports = router;
--- utils/utils.js
+++ utils/utils.js
@@ -1,9 +1,105 @@
-
-exports.getIdFromEmail = function(email) {
+var fs = require('fs-extra');
+var path = require('path');
+var mime = require('mime');
+/**
+ * Extracts the ID from an emailstring
+ * @param String email - The string representation of the email
+ * @returns String id - Userid
+ */
+function getIdFromEmail(email) {
     var id = email.substring(0, email.indexOf("@"));
     return id;
 };
 
-exports.addMinsToDate = function(date, mins) {
+/**
+ * Adds minutes to the date given as 60000ms/min.
+ * This is due to the long integer time that all machines use. 
+ * @param Date date - The date object given
+ * @param Integer mins - The minutes to add
+ * @returns Date with added minutes
+ */
+function addMinsToDate(date, mins) {
     return new Date(date.getTime() + mins*60000);
 };
+
+/**
+ * Returns a generator of fullpaths of files found.
+ * @param String filter - The case sensitive filter to match the file with
+ * @param directory - The full path to search
+ * @returns file - The Full path of the each file
+ */
+function* findFiles(filter,directory) {
+	if(!fs.existsSync(directory)) {
+		console.log("ERR: "+directory+" does not exist");
+		return;
+	}
+	var files = fs.readdirSync(directory);
+	for (var i = 0; i < files.length; ++i) {
+		var filename = path.join(directory,files[i]);
+		var stat = fs.lstatSync(filename);
+		if(!stat.isDirectory() && filename.indexOf(filter) >= 0) {
+			yield filename;
+		}
+	}
+};
+/**
+ * Pipes a file back to the GET Http request <br>
+ * @param HTTPRequest req - the request to receive this file (UNUSED)
+ * @param HTTPResponse res - the response to send this file through
+ * @param String file - the full path of the file. <br>
+ * @returns void
+ */
+function download(req, res, file){
+	fs.exists(file,	function(exist){
+		if(exist){
+			var filename = path.basename(file);
+			var mimetype = mime.lookup(file);
+			res.setHeader('Content-disposition','attachment; filename=',filename);
+			res.setHeader('Content-type',mimetype);			
+			var fstream = fs.createReadStream(file);
+			fstream.pipe(res);
+		} else {
+			res.status(404).send('Content not found');
+		}
+	});
+};
+
+/**
+ * Receives a file from the POST http request
+ * @param HTTPRequest req - The request this file comes from
+ * @param String name - The name to give this file when saving it
+ * @param String dest - The relative path to 'content/images/%dest%'
+ * @returns void
+ */
+function uploadImage(req, name, dest){
+	var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+    	var dir = path.join(__dirname, '../content/images/' + dest + '/');
+    	var fext = path.extname(filename);
+        console.log("Uploading: " + filename);
+        //Path where image will be uploaded
+        fs.exists(dir,function(exist){
+        	if(!exist)
+        		fs.mkdirSync(dir);
+        	else
+        		for (var f of findFiles(name, dir)){
+        			if (f !== undefined)
+        				fs.unlinkSync(f);
+        		}        		
+        	//fstream = fs.createWriteStream(path.join(dir,filename));
+        	//gm(file,filename).write(fstream,function(err) {if (err) throw err;});
+            fstream = fs.createWriteStream(path.join(dir, name + fext));
+            file.pipe(fstream);
+            fstream.on('close', function () {    
+                console.log("Uploaded: " + filename); 
+            });
+        });
+    });
+};
+// Exports
+exports.getIdFromEmail = getIdFromEmail;
+exports.addMinsToDate = addMinsToDate;
+exports.findFiles = findFiles;
+exports.download = download;
+exports.uploadImage = uploadImage;
