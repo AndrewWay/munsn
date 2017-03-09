2690904b83ee594b5ad315ab9a0d955fdc428ac2
kpwh23 <kpwh23@mun.ca>
2017-03-09 17:06:17 -0330

    Updates to Login and Registration
    login.js :
    Removed unnecessary calls .
    Added functions to allow the user to go from Registration to Login.
    Added ability to view image before upload in registration.
    
    login.css:
    Added z-index to elements to remove unnecessary javascript
    Changed button colour.
    
    login.hjs:
    Added back button to go from registration to login.
    Updated profile picture section to allow for image display

--- public/js/login.js
+++ public/js/login.js
@@ -10,13 +10,26 @@ $(document).ready(function(){
 $("#regBtn").click(function(){
 	//Another option would be to animate the entire body changing background and changing z-index
 	$("#obscure").fadeIn(500);
-	$("#loginPanel").css("z-index","2");
 	$("#loginFields").hide()
 	
-$("#loginPanel").animate({width: "400px", right: "-8%"}, 500, function(){$("#loginPanel").animate({height: "600px", top: "3%"}, 500, function(){$("#regFields").fadeIn(300)})}); //Testing is needed for this on many screen sizes
+	$("#loginPanel").animate({width: "400px", right: "-8%"}, 500, function(){$("#loginPanel").animate({height: "600px", top: "3%"}, 500, function(){$("#regFields").fadeIn(300)})}); //Testing is needed for this on many screen sizes
 
+});
 
+$("#closeReg").click(function(){
+	//Another option would be to animate the entire body changing background and changing z-index
 
+	$("#regFields").hide()
+	
+	$("#loginPanel").animate({width: "200px", right: "4%"}, 500, function(){$("#loginPanel").animate({height: "400px", top: "15%"}, 500, function(){$("#loginFields").fadeIn(300)})}); //Testing is needed for this on many screen sizes
+
+	$("#obscure").fadeOut(500);
+
+});
+
+//For now this just shows functionality. Will have to see how integration will work with this.
+$("#picUp").change(function(){
+	$("#picDisp").attr("src", window.URL.createObjectURL(this.files[0]));
 });
 
 
