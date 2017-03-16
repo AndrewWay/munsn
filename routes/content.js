09beb44f11e71a1587658acc320a909000597b4a
John Hollett <g1xb17@gmail.com>
2017-03-16 20:18:38 -0230

    Content for Resumes is added

--- routes/content.js
+++ routes/content.js
@@ -20,13 +20,16 @@ router.get('/image/group/:gid', function (req, res, next) {
 	utils.download(req, res, file);
 });
 
+router.get('/resume/user/:uid', function (req, res, next) {
+	var file = utils.findFiles('resume', path.join(__dirname, '../content/resumes/user/' + req.params.uid)).next().value;
+	utils.download(req, res, file);
+});
 /**
  * Upload a User profile image based on the userid
  * HTMLForm Example: action='/content/image/user/user123'
  */
 router.post('/image/profile/:uid', function (req, res, next) {
 	utils.uploadImage(req, 'user/' + req.params.uid, 'profile');
-	res.redirect('back');
 });
 
 /**
@@ -35,9 +38,14 @@ router.post('/image/profile/:uid', function (req, res, next) {
  */
 router.post('/image/group/:gid', function (req, res, next) {
 	utils.uploadImage(req, 'group/' + req.params.gid, 'group');
-	res.redirect('back');
 });
-
+/**
+ * Upload a User Resume based on the userid
+ * HTMLForm Example: action='/content/resume/user/user123'
+ */
+router.post('/resume/user/:uid', function (req, res, next) {
+	utils.upload(req, 'resumes/user/' + req.params.uid, 'resume');
+});
 /* Unused
 router.post('/image/post/:pid', function(req, res, next) {
 	uploadImage(req, 'posts/'+req.params.pid);
