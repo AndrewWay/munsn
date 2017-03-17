8dcd7766a54d587326971e4dbd7ddbe937f05777
kpwh23 <kpwh23@mun.ca>
2017-03-17 10:33:17 -0230

    Major updates. See description.
    Added two new pages, updated style of old pages to be more consistent.
    
    Pages added:
    group.hjs & profile.hjs: Very similar, used for displaying a user
    profile and a group. Based off blank.hjs, in future will implement it
    such that blank only needs to be loaded once with dynamic changing.
    
    Associated css and js files
    
    login.css & post.css: edited for consitent style

---
+++ public/js/profile.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+	$("#postBox #text").focus(function() {
+	
+		$("#infoContainer").animate({height: "300px"},200);
+		$("#postBox #text").animate({height: "110px"},200);
+	
+	
+	});
+	
+	$("#postBox #text").focusout(function() {
+	
+		$("#infoContainer").animate({height: "220px"},200);
+		$("#postBox #text").animate({height: "30px"},200);
+	
+	
+	});
+});
