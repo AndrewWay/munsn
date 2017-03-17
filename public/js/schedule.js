c12515274c51bd40545b5abceb09084bc9dd85cc
kpwh23 <kpwh23@mun.ca>
2017-03-17 14:49:56 -0230

    Added random events to schedule
    Just for visualization. Might be changing to use a google calendar
    anyways.

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -12,7 +12,25 @@ $(document).ready(function() {
 		navLinks: true, // can click day/week names to navigate views
 		editable: true,
 		eventLimit: true, // allow "more" link when too many events
-		events: [] //Integrate with server.
+		events: [{
+					title: 'All Day Event',
+					start: '2017-03-01'
+				},
+				{
+					title: 'Long Event',
+					start: '2017-03-07',
+					end: '2017-03-10'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2017-03-09T16:00:00'
+				},
+				{
+					id: 999,
+					title: 'Repeating Event',
+					start: '2017-03-16T16:00:00'
+				}] //Integrate with server.
     });
 
 	$(window).scroll(function(){
