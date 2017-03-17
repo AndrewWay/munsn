44da02da222f7b7bd272a1f7d4adebeede4af405
John Hollett <g1xb17@gmail.com>
2017-03-17 20:16:25 -0230

    Content Server DELETE verbs

--- routes/content.js
+++ routes/content.js
@@ -9,7 +9,9 @@ var utils = require('../utils/utils');
  */
 router.get('/image/profile/:uid', function (req, res, next) {
 	var file = utils.findFiles('profile', path.join(__dirname, '../content/images/user/' + req.params.uid)).next().value;
-	utils.download(req, res, file);
+	utils.download(req, res, file, function (result) {
+		res.json(result);
+	});
 });
 /**
  * Return Group profile picture named 'group'
@@ -17,7 +19,9 @@ router.get('/image/profile/:uid', function (req, res, next) {
  */
 router.get('/image/group/:gid', function (req, res, next) {
 	var file = utils.findFiles('group', path.join(__dirname, '../content/images/group/' + req.params.gid)).next().value;
-	utils.download(req, res, file);
+	utils.download(req, res, file, function (result) {
+		res.json(result);
+	});
 });
 /**
  * Return User resume named 'resume.pdf'
@@ -25,7 +29,9 @@ router.get('/image/group/:gid', function (req, res, next) {
  */
 router.get('/resume/user/:uid', function (req, res, next) {
 	var file = utils.findFiles('resume', path.join(__dirname, '../content/resumes/user/' + req.params.uid)).next().value;
-	utils.download(req, res, file);
+	utils.download(req, res, file, function (result) {
+		res.json(result);
+	});
 });
 /**
  * Return a Post Image or other file based on the postid, saves the file as the id for the name
@@ -33,14 +39,19 @@ router.get('/resume/user/:uid', function (req, res, next) {
  */
 router.get('/posts/:pid/:id', function (req, res, next) {
 	var file = utils.findFiles(req.params.id, path.join(__dirname, '../content/posts/' + req.params.pid)).next().value;
-	utils.download(req, res, file);
+	utils.download(req, res, file, function (result) {
+		res.json(result);
+	});
 });
+
 /**
  * Upload a User profile image based on the userid
  * HTMLForm Example: action='/content/image/user/user123'
  */
 router.post('/image/profile/:uid', function (req, res, next) {
-	utils.uploadImage(req, 'user/' + req.params.uid, 'profile');
+	utils.uploadImage(req, 'user/' + req.params.uid, 'profile', function (result) {
+		res.json(result);
+	});
 });
 
 /**
@@ -48,14 +59,18 @@ router.post('/image/profile/:uid', function (req, res, next) {
  * HTMLForm Example: action='/content/image/group/test123'
  */
 router.post('/image/group/:gid', function (req, res, next) {
-	utils.uploadImage(req, 'group/' + req.params.gid, 'group');
+	utils.uploadImage(req, 'group/' + req.params.gid, 'group', function (result) {
+		res.json(result);
+	});
 });
 /**
  * Upload a User Resume based on the userid
  * Resume Example: src='/content/resume/user/user123'
  */
 router.post('/resume/user/:uid', function (req, res, next) {
-	utils.upload(req, 'resumes/user/' + req.params.uid, 'resume');
+	utils.upload(req, 'resumes/user/' + req.params.uid, 'resume', function (result) {
+		res.json(result);
+	});
 });
 /**
  * Upload a Post Image or other file based on the postid, saves the file as the id for the name
@@ -63,7 +78,9 @@ router.post('/resume/user/:uid', function (req, res, next) {
  * Example: action='/content/posts/post123/any123'
  */
 router.post('/posts/:pid/:id', function (req, res, next) {
-	utils.upload(req, 'posts/' + req.params.pid + '/', req.params.id);
+	utils.upload(req, 'posts/' + req.params.pid + '/', req.params.id, function (result) {
+		res.json(result);
+	});
 });
 /* Unused
 router.post('/image/post/:pid', function(req, res, next) {
@@ -71,5 +88,40 @@ router.post('/image/post/:pid', function(req, res, next) {
 	res.redirect('back');
 });
 */
-
+/**
+ *
+ */
+router.delete('/resume/user/:uid', function (req, res, next) {
+	if (utils.remove('resume', path.join(__dirname, '../content/resumes/user/' + req.params.uid))) {
+		res.json({
+			status: 'ok'
+		});
+	} else {
+		res.json({
+			status: 'fail'
+		});
+	}
+});
+router.delete('/image/profile/:uid', function (req, res, next) {
+	if (utils.remove('profile', path.join(__dirname, '../content/images/user/' + req.params.uid))) {
+		res.json({
+			status: 'ok'
+		});
+	} else {
+		res.json({
+			status: 'fail'
+		});
+	}
+});
+router.delete('/image/group/:gid', function (req, res, next) {
+	if (utils.remove('group', path.join(__dirname, '../content/images/user/' + req.params.gid))) {
+		res.json({
+			status: 'ok'
+		});
+	} else {
+		res.json({
+			status: 'fail'
+		});
+	}
+});
 module.exports = router;
