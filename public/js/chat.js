d892858ba68a6ee2681443ed91e259ae5ddb631f
Devin M <devinm_1919@hotmail.com>
2017-03-16 23:44:46 -0230

    Chat: - See when user connects/disconnects - Send messages to all available users

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-var socket = io();
+$(function () {
+    var socket = io();
+    $('form').submit(function(){
+      socket.emit('chat message', $('#m').val());
+      $('#m').val('');
+      return false;
+    });
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+    });
+});
--- utils/socket.js
+++ utils/socket.js
@@ -1,5 +1,23 @@
 var IO = require('../bin/www');
 
-IO.on('connection', function() {
-    console.log("SOCKET.IO: Connected");
+var users = [];
+
+//User connects to server
+IO.on('connection', function(socket) {
+    users.push(socket);
+    console.log("SOCKET.IO: User with id " + socket.id + " connected");
+    console.log("SOCKET.IO: " + users.length);
+
+    //User disconnects
+    socket.on('disconnect', function() {
+        var index = users.indexOf(socket);
+        if (index != -1) {
+            users.splice(index, 1);
+            console.log("SOCKET.IO: User with id " + socket.id + " disconnected");
+        }
+    });
+    socket.on('chat message', function(msg){
+        IO.emit('chat message', msg);
+        console.log('message: ' + msg);
+    });
 });
