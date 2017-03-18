b1570377614cfa34201c4eab4ba1f01eec160140
Devin M <devinm_1919@hotmail.com>
2017-03-18 13:36:40 -0230

    App.js: - Added new routes for profile, group, schedule, settings
    /routes:
    - Added new router files for prodile, group, schedule, settings

---
+++ routes/settings.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+var DB = require('../utils/db');
+
+//TODO:
+//This currently shows the profile regardless if logged in 
+//Need to add templating in so that functionality changes based if the user is logged in or not
+router.get('/', function (req, res, next) {
+	if (req.session.user) {
+    	res.render('settings', {});
+	}
+	else {
+		res.render('settings', {});
+	}
+});
+
+module.exports = router;
