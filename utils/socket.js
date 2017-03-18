fcfa03066011d659f5cede14ad3a56a93a779ceb
Devin M <devinm_1919@hotmail.com>
2017-03-18 16:14:46 -0230

    Chat: - Now has support of nicknames, which should lead into DB usage - To set a nick name type "/name name"

--- utils/socket.js
+++ utils/socket.js
@@ -4,23 +4,59 @@ var users = [];
 
 //User connects to server
 IO.on('connection', function(socket) {
-    users.push(socket);
-    console.log("SOCKET.IO: User with id " + socket.id + " connected");
-    console.log("SOCKET.IO: " + users.length);
+    users.push({
+        socket: socket,
+        name: socket.id
+    });
+    console.log("[CHAT]: User with id " + socket.id + " connected");
+    console.log("[CHAT]: There are currently " + users.length + " users");
     IO.emit('chat message', "User: " + socket.id + " has joined!");
+
+    //Name
+    socket.on('name', function(data, callback) {
+        console.log("NAME:" + data);
+        var index = usersIndexOf(socket);
+        console.log("INDEX: " + index);
+        if (index != -1) {
+            users[index].name = data;
+            callback({status: "ok"});
+        }
+        else {
+            callback({status: "fail"});
+        }
+    });
 
     //User disconnects
     socket.on('disconnect', function() {
-        var index = users.indexOf(socket);
+        var index = usersIndexOf(socket);
         if (index != -1) {
+            console.log("[CHAT]: User with id " + socket.id + " disconnected");
+            IO.emit('chat message', users[index].name + " has left!");
             users.splice(index, 1);
-            console.log("SOCKET.IO: User with id " + socket.id + " disconnected");
-            IO.emit('chat message', "User: " + socket.id + " has left!");
         }
     });
+
+    //Chat message
     socket.on('chat message', function(msg){
-        var name = users[users.indexOf(socket)].id;
-        IO.emit('chat message', name + ": " + msg);
-        console.log('message: ' + name + ": " + msg);
+        var name;
+        var index = usersIndexOf(socket);
+        if (index != -1) {
+            IO.emit('chat message', users[index].name + ": " + msg);
+            console.log('[CHAT] ' + users[index].name + ": " + msg);
+        }
+        else {
+            IO.emit('chat message', socket.id + ": " + msg);
+            console.log('[CHAT] ' + socket.id + ": " + msg);           
+        }
     });
 });
+
+/**
+ * Gets the index of users with a matching socket
+ * 
+ * @param {any} search - The search term
+ * @returns - Index
+ */
+function usersIndexOf(search) {
+    return users.map(function(e) {return e.socket;}).indexOf(search);
+}
