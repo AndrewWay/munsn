5086ef5cb03d0f13cde8373f7d5cba3569689a09
kpwh23 <kpwh23@mun.ca>
2017-03-19 21:54:27 -0230

    Add comments to setting.js
    Known issue is that if you're zoomed in too much this js doesn't work

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-
+	
+	/******************
+	* Dropbox
+	*
+	*@params: null
+	*
+	* Functions for expanding and contracting the option boxes
+	******************/
+	
+	
+	//Expand when contracted, contract when expanded
 	$(".setTitle").click(function (sett) {
 		if ($(this).parent().height() < $(this).height() + $(this).next().height()) {
 			$(this).parent().animate({
