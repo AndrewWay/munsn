5502f6f61f6337c9c10f83470dc5022a33afe308
John Hollett <g1xb17@gmail.com>
2017-03-22 20:15:20 -0230

    Ugh missed a few

--- utils/DB/socket.js
+++ utils/DB/socket.js
@@ -23,12 +23,12 @@ module.exports = function (DBSocket, collectionSocket) {
 
 	//Find users matching query
 	DBSocket.findSid = function (socketid, callback) {
-		console.log("[DBSocket] Find: '" + socketid + "'");
+		console.log("[DBSocket] FindSid", "'" + socketid + "'");
 		collectionSocket.findOne({
 			socketid: socketid
 		}, function (err, result) {
 			if (err) {
-				console.error("[DBSocket] Find: " + err.message);
+				console.error("[DBSocket] Find", err.message);
 				callback(err, result);
 			} else {
 				callback(err, result);
@@ -38,12 +38,12 @@ module.exports = function (DBSocket, collectionSocket) {
 
 	//Find users matching query
 	DBSocket.findUid = function (uid, callback) {
-		console.log("[DBSocket] Find: '" + uid + "'");
+		console.log("[DBSocket] FindUid", "'" + uid + "'");
 		collectionSocket.findOne({
 			_id: uid
 		}, function (err, result) {
 			if (err) {
-				console.error("[DBSocket] Find: " + err.message);
+				console.error("[DBSocket] Find", err.message);
 				callback(err, result);
 			} else {
 				callback(err, result);
