2449d7fea9b38c422f5cfac5500d9a908b05106f
kpwh23 <kpwh23@mun.ca>
2017-04-01 18:26:30 -0230

    Working on schedule
    Added:
    nlp.js
    rrule.js
    
    To configure rrules.
    
    Everything else is updates for schedule

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,50 +1,62 @@
 $(document).ready(function () {
 
-	// page is now ready, initialize the calendar...
+	/***************************
+	 * Get calendar.
+	 * 
+	 * @params: uid
+	 * 
+	 * Gets users associated calendar and place in page.
+	 ****************************/
 
-	$('#calendar').fullCalendar({
-		header: {
-			left: 'prev,next today',
-			center: 'title',
-			right: 'month,basicWeek,basicDay'
-		},
-		navLinks: true, // can click day/week names to navigate views
-		editable: true,
-		eventLimit: true, // allow "more" link when too many events
-		events: [{
-				title: 'All Day Event',
-				start: '2017-03-01'
-			},
-			{
-				title: 'Long Event',
-				start: '2017-03-07',
-				end: '2017-03-10'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: '2017-03-09T16:00:00'
-			},
-			{
-				id: 999,
-				title: 'Repeating Event',
-				start: '2017-03-16T16:00:00'
-			}
-		] //Integrate with server.
-	});
-
-	$(window).scroll(function () {
-
-		console
-
-		if (($(".sidebars").offset().top + $(".sidebars").height()) < $("#calendar").height() || $(this).scrollTop() < $(".sidebars").offset().top) {
-			$(".sidebars").css({
-
-				'top': $(this).scrollTop() + 50 //Use it later
-
-
-			});
+	//Attempt to make a new calendar.
+	//TODO: Should this be made on registration? Probably.
+	$.post("/content/calendar/user/session",function(result){
+		if(result.data){
+			$("#calendar").html('<iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src='+result.data.id+'&amp;color=%230F4B38&amp;ctz=America%2FSt_Johns" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>');
 		}
+		
 	});
+
+	//Get the calendar and display it in page.
+	$.get("/content/calendar/user/session", function(result){
+		if(result.data){
+				$("#calendar").html('<iframe src="https://calendar.google.com/calendar/embed?showPrint=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src='+result.data.id+'&amp;color=%230F4B38&amp;ctz=America%2FSt_Johns" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>');
+		}
+		
+	});
+	
+	/**********************
+	 * DateTimePicker
+	 * 
+	 * @params: null
+	 * 
+	 * Initialize the data and time picker for events
+	 **********************/
+	 
+	 //Initialize end time picker
+	 $('#endTime').datetimepicker({
+ 		 startDate:'0',
+ 		 minTime:new Date()
+	 });
+
+	 //Intialize start time
+	 $('#startTime').datetimepicker({
+		 startDate:'0',
+ 		 minTime:new Date(),
+	 });
+
+
+
+	/**********************
+	 * Add events
+	 * 
+	 * @params: null
+	 * 
+	 * RRule stuff
+	 **********************/
+
+	 var days, 
+
+	days = [RRule.MO, RRule.TU, RRule.WE, RRule.TH, RRule.FR, RRule.SA, RRule.SU];
 
 });
