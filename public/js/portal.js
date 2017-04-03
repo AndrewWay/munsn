85157ee53c56a8fd7d0a2f2c9401087bf8ca07b4
LAPTOP-DEVIN\Devin <dfcm15@mun.ca>
2017-04-03 02:37:51 -0700

    working on search

--- public/js/portal.js
+++ public/js/portal.js
@@ -3,6 +3,14 @@ var imgBool = false;
 
 $(document).ready(function () {
 
+    $('#searchbtn').click(function() {
+        $.get('/api/search', {query: $('#searchbar').val()}, function(results) {
+            alert(results);
+            console.log("SEARCH:" + results);
+        });
+    });
+
+
 	/******************
 	* Post box expansion.
 	*
