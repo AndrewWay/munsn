759f6ced11a377d3d15a0c8df1130773568f65a1
LAPTOP-DEVIN\Devin <dfcm15@mun.ca>
2017-04-02 20:43:57 -0700

    Fixed the loadMessages command bug

--- utils/DB/search.js
+++ utils/DB/search.js
@@ -3,7 +3,40 @@ var ObjectID = require('mongodb').ObjectID;
 module.exports = function (DBSearch, collectionUsers, collectionGroups, collectionCourses) {
     
     DBSearch.search = function(req, res, callback) {
-        
+        if (req.body.query) {
+            var query = req.body.query;
+            var searchResults = [];
+            // Declare loop control variables
+            var cbUsers = false, cbGroups = false, cbCourses = false, masterBreak = false;
+            //Search through users
+            collectionUsers.find().toArray(function(err, resultsUsers) {
+                if (err) {
+                    console.error("[DBPosts] Add->'" + post.type + "'", err.message);
+                }
+            });
+            //Search through groups
+            collectionGroups.find().toArray(function(err, resultsGroups) {
+                if (err) {
+                    console.error("[DBPosts] Add->'" + post.type + "'", err.message);
+                } 
+            });
+            //Search through courses
+            collectionCourses.find().toArray(function(err, resultsCourses) {
+                if (err) {
+                    console.error("[DBPosts] Add->'" + post.type + "'", err.message);
+                }
+            });
+            while((!cbUsers && !cbGroups && !cbCourses) || !masterBreak) {
+
+            }
+        }
+        else {
+            console.warn("[DBSearch] search", "'Missing Fields'");
+			callback({
+				session: req.session,
+				status: "fail"
+			});
+        }
     };
 
 };
