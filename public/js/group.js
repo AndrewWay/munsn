3504759c74147c6b307ea6ed36174103982636c4
kpwh23 <kpwh23@mun.ca>
2017-04-04 22:02:22 -0230

    More group fixes
    Can now send requests and shows suggested friends

--- public/js/group.js
+++ public/js/group.js
@@ -206,43 +206,6 @@ $(document).ready(function () {
 			//TODO: Function on failures.
 		);
 
-	/***********************
-	 * Resume button
-	 *
-	 *@params: null
-	 *
-	 * Button linking to resume page.
-	 ************************/
-
-	//When button is clicked go to resume page.
-	$('#infoButton #resume').click(function () {
-		window.location.href = "/resume/" + id;
-	});
-
-	/************************
-	 * Suggested friend buttons
-	 *
-	 *@params: null
-	 *
-	 * Behaviour of suggested friend box when a button is clicked.
-	 *
-	 *COMMENTS: Should be moved to something outside here so there is no repeated code.
-	 ************************/
-
-	//When Previous button is clicked move backwards through list of suggested friends.
-	$('#suggPrev').click(function () {
-		//TODO -- Implementation when integration is done
-
-
-	});
-
-	//When Next button is clicked move forwards through list of suggested friends.
-	$('#suggNext').click(function () {
-		//TODO -- Implementation when integration is done
-
-
-	});
-
 	//TODO: Potentially move to it's own file to be accessed by every page that needs it.
 	/*******************
 	 * Load group info
