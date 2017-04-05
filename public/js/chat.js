b7829953aca19a18d7fd65709bdaed54fbda7e08
LAPTOP-DEVIN\Devin <dfcm15@mun.ca>
2017-04-05 01:24:22 -0700

    ferfergeg

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,6 +19,7 @@ $(function () {
                 friendId = $(window.parent.location).attr('href').substring($(window.parent.location).attr('href').indexOf("/profile#") + "/profile#".length);
                 console.log(window.parent.location.href.substring(window.parent.location.href.indexOf("/profile/#") + "/profile/#".length));
                 console.log($(window.parent.location).attr('href').substring($(window.parent.location).attr('href').indexOf("/profile#") + "/profile#".length));
+                console.log("friend: " + friendId);
                 $.get('/api/messages', {uid1: sess.user._id, uid2: friendId}, function(data) {
                     console.log(JSON.stringify(data));
                     for (var i = 0; i < data.data[0].messages.length; i++) {
